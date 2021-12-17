b810e9b70359fbec5eba2bcc5b5417ff8e9c35ec
Yasir Ali <aliyasirali@gmail.com>
2021-12-17 15:45:23 +0100

    carousel basic impl added

--- src/app/media/media.component.ts
+++ src/app/media/media.component.ts
@@ -6,36 +6,52 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./media.component.scss']
 })
 export class MediaComponent implements OnInit {
-  public list = [];
+  public images = [];
+  slideIndex = 0;
   constructor() { }
-
   ngOnInit() {
-    this.list = [
-      { rows: 2, cols: 2, data: 1 },
-      { rows: 1, cols: 1, data: 2 },
-      { rows: 2, cols: 1, data: 3 },
-      { rows: 1, cols: 1, data: 4 },
-      { rows: 1, cols: 1, data: 4 },
-
-      { rows: 1, cols: 1, data: 5 },
-      { rows: 2, cols: 2, data: 6 },
-      { rows: 1, cols: 1, data: 7 },
-      { rows: 1, cols: 1, data: 8 },
-      { rows: 1, cols: 1, data: 9 },
-
-      { rows: 1, cols: 1, data: 10 },
-      { rows: 1, cols: 1, data: 11 },
-      { rows: 2, cols: 2, data: 12 },
-      { rows: 1, cols: 1, data: 13 },
-      { rows: 1, cols: 1, data: 14 },
-
-      { rows: 1, cols: 1, data: 5 },
-      { rows: 2, cols: 2, data: 6 },
-      { rows: 1, cols: 1, data: 7 },
-      { rows: 1, cols: 1, data: 8 },
-      { rows: 1, cols: 1, data: 9 },
-      
+    this.images = [
+      {'url': 'https://picsum.photos/seed/1/800/600'}, 
+      {'url': 'https://picsum.photos/seed/2/600/800'},
+      {'url': 'https://picsum.photos/seed/3/800/800'}, 
+      {'url': 'https://picsum.photos/seed/4/800/600'}, 
+      {'url': 'https://picsum.photos/seed/5/600/800'}
     ];
+  }
+  
+  openModal() {
+    document.getElementById('imgModal').style.display = "block";
+  }
+
+  closeModal() {
+    document.getElementById('imgModal').style.display = "none";
+  }
+
+  plusSlides(n) {
+    this.showSlides(this.slideIndex += n);
+  }
+
+  currentSlide(n) {
+    this.showSlides(this.slideIndex = n);
+  }
+
+  showSlides(slideIndex);
+  showSlides(n) {
+    let i;
+    const slides = document.getElementsByClassName("img-slides") as HTMLCollectionOf<HTMLElement>;
+    const dots = document.getElementsByClassName("images")as HTMLCollectionOf<HTMLElement>;
+    if (n > slides.length) {this.slideIndex = 1}
+    if (n < 1) {this.slideIndex = slides.length}
+    for (i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none";
+    }
+    for (i = 0; i < dots.length; i++) {
+        dots[i].className = dots[i].className.replace(" active", "");
+    }
+    slides[this.slideIndex-1].style.display = "block";
+    if (dots && dots.length > 0) {
+      dots[this.slideIndex-1].className += " active";
+    }
   }
 
 }
